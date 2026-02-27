fd0aa3927b6ed0c8c0d79c55e57260147503cfad
alex <alexandr.svetliy@icloud.com>
2026-02-26 21:23:31 -0700

    feat: solve challenge 02 - age calculator

---
+++ challenge_02_age_calculator.py
@@ -0,0 +1,8 @@
+birth_year_input = input("Enter your birth year: ")
+
+birth_year = int(birth_year_input)
+
+current_year = 2025
+age = current_year - birth_year
+
+print(f"You are approximately {age} years old.")
